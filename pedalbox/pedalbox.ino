e0605f12ca89cad8c4a193268b9fbddfb3659998
Ben Esposito <ben@glodonusa.com>
2022-08-27 22:38:31 -0400

    Add .clang-format

--- pedalbox/pedalbox.ino
+++ pedalbox/pedalbox.ino
@@ -5,36 +5,42 @@ float sensorDifference;
 float differenceAverage;
 
 struct poten_range {
-	int lower_bound;
-	int upper_bound;
+    int lower_bound;
+    int upper_bound;
 };
 
 struct poten_range left_poten_range = {
-	.lower_bound = 367,
-	.upper_bound = 1023,
+    .lower_bound = 367,
+    .upper_bound = 1023,
 };
 
 struct poten_range right_poten_range = {
-	.lower_bound = 104,
-	.upper_bound = 1023,
+    .lower_bound = 104,
+    .upper_bound = 1023,
 };
 
 void setup() {
-	Serial.begin(9600);
-	pinMode(LEFT_POTEN_PIN, INPUT);
-	pinMode(RIGHT_POTEN_PIN, INPUT);
+    Serial.begin(9600);
+    pinMode(LEFT_POTEN_PIN, INPUT);
+    pinMode(RIGHT_POTEN_PIN, INPUT);
 }
 
 void loop() {
-	int left_poten_value = analogRead(LEFT_POTEN_PIN);
-	int right_poten_value = analogRead(RIGHT_POTEN_PIN);
+    int left_poten_value = analogRead(LEFT_POTEN_PIN);
+    int right_poten_value = analogRead(RIGHT_POTEN_PIN);
 
-	int left_sensor_percentage = 100 - map(left_poten_value, left_poten_range.lower_bound, left_poten_range.upper_bound, 0, 100);
-	int right_sensor_percentage = 100 - map(right_poten_value, right_poten_range.lower_bound, right_poten_range.upper_bound, 0, 100);
+    int left_sensor_percentage =
+        100 - map(left_poten_value, left_poten_range.lower_bound,
+                  left_poten_range.upper_bound, 0, 100);
+    int right_sensor_percentage =
+        100 - map(right_poten_value, right_poten_range.lower_bound,
+                  right_poten_range.upper_bound, 0, 100);
 
-	char buf[64];
-	snprintf(buf, sizeof(buf), "[%d, %d] [%d, %d]\n", left_poten_value, right_poten_value, left_sensor_percentage, right_sensor_percentage);
-	Serial.println(buf);
+    char buf[64];
+    snprintf(buf, sizeof(buf), "[%d, %d] [%d, %d]\n", left_poten_value,
+             right_poten_value, left_sensor_percentage,
+             right_sensor_percentage);
+    Serial.println(buf);
 }
 #if 0
 	// maps analog values of Sensor1 and Sensor2 from 0,100
