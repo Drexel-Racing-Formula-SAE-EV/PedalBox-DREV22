99b5b39c0c7cd85418ad095de873387ec3873737
ColeBardin <cab572@drexel.edu>
2022-10-25 19:31:09 -0400

    added length option to set msg data func

--- src/pedalbox.cpp
+++ src/pedalbox.cpp
@@ -19,8 +19,8 @@
 #define LEFT_POTEN_PIN A1
 #define RIGHT_POTEN_PIN A2
 
-#define RxID 0x201  // CAN recieving addr for motor controller
-#define TORQUE_CMD 0x90 
+#define RxID        0x201   // CAN recieving addr for motor controller
+#define TORQUE_CMD  0x90    // Register for TRQ command to be set on motorcontroller
 
 const struct poten_range left_poten_range = {
     .lower_bound = LPOT_MIN,
@@ -37,7 +37,7 @@ unsigned long last_write = millis();
 poten left_poten = poten{LEFT_POTEN_PIN, left_poten_range};
 poten right_poten = poten{RIGHT_POTEN_PIN, right_poten_range};
 
-uint8_t testData[] = {TORQUE_CMD, 0xFC, 0x3F, 0x00, 0x00, 0x00, 0x00, 0x00};
+uint8_t testData[8];
 
 drev_can::can_node canBus(RxID);
 
@@ -46,28 +46,27 @@ drev_can::can_message canMSG {
     .length=3,
 };
 
-
-
-void set_msg_data(drev_can::can_message msgObj, uint8_t *newData) {
-    for (int i=0; i<8; i++){
+void set_msg_data(drev_can::can_message msgObj, uint8_t *newData, uint8_t len) {
+    for (int i=0; i<len; i++){
         msgObj.data[i] = newData[i];
     }
 }
-
 
 void setup() {
     Serial.begin(9600);
     pinMode(LEFT_POTEN_PIN, INPUT);
     pinMode(RIGHT_POTEN_PIN, INPUT);
+    testData[0] = TORQUE_CMD;
+    testData[1] = 0xFC;
+    testData[2] = 0x3F;
 }
 
 void loop() {
     if (millis() - last_write > PERIOD){
         //trq = (left_poten.read_percent() + right_poten.read_percent()) / 2;
 
-        set_msg_data(canMSG, testData);
+        set_msg_data(canMSG, testData, 3);
 
         Serial.println(canBus.send(canMSG));
         last_write = millis();
     }
-}
